4254f92ddf8daa9f73f76109bb34b22daf789d99
Nahuel Rodriguez Viana <nahuelviana1@gmail.com>
2023-05-14 11:36:23 -0300

    carrito crado

--- funciones.js
+++ funciones.js
@@ -7,8 +7,10 @@ const productos = [
     imagen: "../imgs/buzos/buzo black and white.jpg",
     categoria: {
       id: "buzos",
+      nombre: "buzos",
     },
-    precio: 2000,
+    tallas: ["S", "M", "L"],
+    precios: { S: 2000, M: 2400, L: 2500 },
   },
   {
     id: "buzo02",
@@ -16,8 +18,10 @@ const productos = [
     imagen: "../imgs/buzos/buzo blue.jpg",
     categoria: {
       id: "buzos",
+      nombre: "buzos",
     },
-    precio: 2300,
+    tallas: ["S", "M", "L"],
+    precios: { S: 2500, M: 2500, L: 2800 },
   },
   {
     id: "buzo3",
@@ -25,8 +29,10 @@ const productos = [
     imagen: "../imgs/buzos/buzo boreal.jpg",
     categoria: {
       id: "buzos",
+      nombre: "buzos",
     },
-    precio: 2200,
+    tallas: ["S", "M", "L"],
+    precios: { S: 3000, M: 3250, L: 3500 },
   },
   {
     id: "pantalon1",
@@ -34,8 +40,10 @@ const productos = [
     imagen: "../imgs/pantalones/pantalon black.jpg",
     categoria: {
       id: "pantalones",
+      nombre: "pantalones",
     },
-    precio: 1900,
+    tallas: ["S", "M", "L"],
+    precios: { S: 1900, M: 2100, L: 2400 },
   },
   {
     id: "pantalon2",
@@ -43,8 +51,10 @@ const productos = [
     imagen: "../imgs/pantalones/pantalon brown.jpg",
     categoria: {
       id: "pantalones",
+      nombre: "pantalones",
     },
-    precio: 1800,
+    tallas: ["S", "M", "L"],
+    precios: { S: 2100, M: 2300, L: 2450 },
   },
   {
     id: "pantalon3",
@@ -52,8 +62,10 @@ const productos = [
     imagen: "../imgs/pantalones/pantalon jean.jpg",
     categoria: {
       id: "pantalones",
+      nombre: "pantalones",
     },
-    precio: 1950,
+    tallas: ["S", "M", "L"],
+    precios: { S: 4000, M: 4100, L: 4200 },
   },
   {
     id: "remera1",
@@ -61,8 +73,10 @@ const productos = [
     imagen: "../imgs/remeras/remera fuking.jpg",
     categoria: {
       id: "remeras",
+      nombre: "remeras",
     },
-    precio: 1300,
+    tallas: ["S", "M", "L"],
+    precios: { S: 1500, M: 1800, L: 2000 },
   },
   {
     id: "remera2",
@@ -70,8 +84,10 @@ const productos = [
     imagen: "../imgs/remeras/remera leave the road.jpg",
     categoria: {
       id: "remeras",
+      nombre: "remeras",
     },
-    precio: 1500,
+    tallas: ["S", "M", "L"],
+    precios: { S: 2000, M: 2000, L: 2000 },
   },
   {
     id: "remera3",
@@ -79,8 +95,10 @@ const productos = [
     imagen: "../imgs/remeras/remera out cast.jpg",
     categoria: {
       id: "remeras",
+      nombre: "remeras",
     },
-    precio: 1400,
+    tallas: ["S", "M", "L"],
+    precios: { S: 2300, M: 2500, L: 2800 },
   },
   {
     id: "zapatilla1",
@@ -88,8 +106,10 @@ const productos = [
     imagen: "../imgs/zapatillas/zapatilla orange and white.jpg",
     categoria: {
       id: "zapatillas",
+      nombre: "zapatillas",
     },
-    precio: 5000,
+    tallas: ["S", "M", "L"],
+    precios: { S: 3000, M: 3500, L: 3600 },
   },
   {
     id: "zapatilla2",
@@ -97,8 +117,10 @@ const productos = [
     imagen: "../imgs/zapatillas/zapatilla red and white.jpg",
     categoria: {
       id: "zapatillas",
+      nombre: "zapatillas",
     },
-    precio: 7000,
+    tallas: ["S", "M", "L"],
+    precios: { S: 3500, M: 3600, L: 3700 },
   },
   {
     id: "zapatilla3",
@@ -106,38 +128,102 @@ const productos = [
     imagen: "../imgs/zapatillas/zapatilla green and white.jpg",
     categoria: {
       id: "zapatillas",
+      nombre: "zapatillas",
     },
-    precio: 6000,
+    tallas: ["S", "M", "L"],
+    precios: { S: 4000, M: 4500, L: 5000 },
   },
 ];
 
+//realizo la carga de los productos al dom
+
 const contenedorproductos = document.querySelector("#contenedor-productos");
-const productosboton = document.querySelectorAll(".boton-categoria");
+let botonesagregar = document.querySelectorAll(".producto-agregar");
 
 function cargarproductos(productoselegidos) {
-  contenedorproductos.innerHTML = " ";
+  contenedorproductos.innerHTML = "";
   productoselegidos.forEach((producto) => {
     const div = document.createElement("div");
     div.classList.add("producto");
     div.innerHTML = `
-    <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+    <img class="producto-imagen" src="${producto.imagen}" alt="${
+      producto.titulo
+    }">
     <div class="producto-detalles">
       <h3 class="producto-titulo">${producto.titulo}</h3>
-      <p class="producto-precio">$${producto.precio}</p>
+      <div class="producto-tallas">
+        <p>Tallas:</p>
+        <ul class="tallas-lista">
+          ${producto.tallas
+            .map(
+              (talla) => `
+            <li>
+              <span class="talla"><input type="checkbox" name="myCheckbox" value="myValue">-${talla}-</span>
+              <span class="precio">-$${producto.precios[talla]}</span>
+            </li>
+          `
+            )
+            .join("")}
+        </ul>
+      </div>
       <button class="producto-agregar" id="${producto.id}">Agregar</button>
     </div>
     `;
     contenedorproductos.append(div);
   });
+  nuevosbotonesagregar();
 }
 
+//array de categorias y botones
+
+const productosboton = document.querySelectorAll(".boton-categoria");
+const tituloprincipal = document.getElementById("titulo-principal");
 cargarproductos(productos);
 
 productosboton.forEach((boton) => {
   boton.addEventListener("click", (e) => {
-    const productosboton = productos.filter(
-      (producto) => producto.categoria.id === e.currentTarget.id
-    );
-    cargarproductos(productosboton);
+    if (e.currentTarget.id != "todos") {
+      const productocategoria = productos.find(
+        (producto) => producto.categoria.id === e.currentTarget.id
+      );
+      tituloprincipal.innerText = productocategoria.categoria.nombre;
+      const productosboton = productos.filter(
+        (producto) => producto.categoria.id === e.currentTarget.id
+      );
+      cargarproductos(productosboton);
+    } else {
+      tituloprincipal.innerText = "todos los productos";
+      cargarproductos(productos);
+    }
   });
 });
+
+// botones de articulos
+
+function nuevosbotonesagregar() {
+  botonesagregar = document.querySelectorAll(".producto-agregar");
+}
+
+function nuevosbotonesagregar() {
+  botonesagregar = document.querySelectorAll(".producto-agregar");
+  botonesagregar.forEach((boton) => {
+    boton.addEventListener("click", AgregarAlCarrito);
+  });
+}
+
+//creando carrito y agregando productos
+
+const productosEnCarrito = [];
+
+function AgregarAlCarrito(e) {
+  const idboton = e.currentTarget.id;
+  const productoAgregado = productos.find(
+    (producto) => producto.id === idboton
+  );
+  if (productosEnCarrito.some((producto) => producto.id === idboton)) {
+  } else {
+    productoAgregado.cantidad = 1;
+    productosEnCarrito.push(productoAgregado);
+  }
+  console.log(productosEnCarrito);
+}
